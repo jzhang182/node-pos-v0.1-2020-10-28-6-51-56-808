f619dfd7648c0853e6d40702f1c588cc836db421
Jialong ZHANG <JZhang182@slb.com>
2020-11-06 16:45:13 +0800

    Adjust Template Strings

--- main/main.js
+++ main/main.js
@@ -1,32 +1,28 @@
 module.exports = function main(inputArray) {
     let ret = '';
     let totalCost = 0;
-    const groupBy = (array, key) => {
-        return array.reduce((result, currentValue) => {
-            (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
-            return result;
-        }, {});
-    };
-    let groupedArray = groupBy(inputArray, 'Barcode')
+    const grouped = inputArray.reduce((result, currentValue) => {
+        (result[currentValue[`Barcode`]] = result[currentValue[`Barcode`]] || []).push(currentValue);
+        return result;
+    }, {})
 
     ret += '***<store earning no money>Receipt ***\n';
 
-    for (let c in groupedArray) {
-        let quantity = groupedArray[c].length;
-        let subtotal = quantity * groupedArray[c][0].Price;
+    for (let c in grouped) {
+        let subtotal = grouped[c].length * grouped[c][0].Price;
         totalCost += subtotal;
-        if (groupedArray[c][0].Unit != 'a') {
-            ret += 'Name: ' + groupedArray[c][0].Name + ', Quantity: ' + quantity + ' ' + groupedArray[c][0].Unit + 's, Unit price: '
-                + groupedArray[c][0].Price.toFixed(2) + ' (yuan), Subtotal: ' + subtotal.toFixed(2) + ' (yuan)\n';
+        if (grouped[c][0].Unit != 'a') {
+            ret += `Name: ${grouped[c][0].Name}, Quantity: ${grouped[c].length} ${grouped[c][0].Unit}s,`
+                + ` Unit price: ${grouped[c][0].Price.toFixed(2)} (yuan), Subtotal: ${subtotal.toFixed(2)} (yuan)\n`;
         }
         else {
-            ret += 'Name: ' + groupedArray[c][0].Name + ', Quantity: ' + quantity + ', Unit price: '
-                + groupedArray[c][0].Price.toFixed(2) + ' (yuan), Subtotal: ' + subtotal.toFixed(2) + ' (yuan)\n';
+            ret += `Name: ${grouped[c][0].Name}, Quantity: ${grouped[c].length},`
+                + ` Unit price: ${grouped[c][0].Price.toFixed(2)} (yuan), Subtotal: ${subtotal.toFixed(2)} (yuan)\n`;
         }
     }
 
-    ret += '----------------------\n';
-    ret += 'Total: ' + totalCost.toFixed(2) + ' (yuan)\n'
-    ret += '**********************\n';
+    ret += '----------------------\n'
+        + `Total: ${totalCost.toFixed(2)} (yuan)\n`
+        + '**********************\n';
     return ret;
 }
